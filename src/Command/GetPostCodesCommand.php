f4943e0262605730a4fdb99388228f84986a7c9f
Jakub Holy <jakub.holy@gmail.com>
2017-08-25 17:21:44 +0200

    nicer output and error handling

--- src/Command/GetPostCodesCommand.php
+++ src/Command/GetPostCodesCommand.php
@@ -8,10 +8,18 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use BeSimple\SoapClient\SoapClientBuilder;
 use BeSimple\SoapClient\SoapClientOptionsBuilder;
+use BeSimple\SoapClient\SoapClient;
 use BeSimple\SoapCommon\SoapOptionsBuilder;
 use Symfony\Component\DomCrawler\Crawler;
+use SoapFault;
+
 
 final class GetPostCodesCommand extends Command {
+
+    const METHOD_NAME = 'GetUKLocationByTown';
+    const WSDL_URL = 'http://www.webservicex.net/uklocation.asmx?WSDL';
+    const RESPONSE_ITEM_XPATH = '//table';
+    const RESPONSE_DATA_XPATH = '//table/*';
 
     protected function configure() {
         $this
@@ -28,44 +36,65 @@ final class GetPostCodesCommand extends Command {
     }
 
     protected function execute(InputInterface $input, OutputInterface $output) {
+        try {
+            $soapClientBuilder = new SoapClientBuilder();
+            $soapClient = $soapClientBuilder->build(
+                SoapClientOptionsBuilder::createWithDefaults(),
+                SoapOptionsBuilder::createWithDefaults(self::WSDL_URL)
+            );
+        }
+        catch (SoapFault $sfex) {
+            echo 'WSDL unavailable or broken.'."\n";
+            error_log($sfex->getMessage());
+            exit(1);
+        }
+        try {
+            $townData = $this->requestPostCodeByTown('London', $soapClient);
+            $this->echoPostCodes($townData);
+            $townData = $this->requestPostCodeByTown('Manchester', $soapClient);
+            $this->echoPostCodes($townData);
 
+        }
+        catch (SoapFault $sfex) {
+            echo 'SOAP request failed.'."\n";
+            error_log($sfex->getMessage());
+            exit(2);
+        }
 
-
-
-
-        $soapClientBuilder = new SoapClientBuilder();
-        $soapClient = $soapClientBuilder->build(
-            SoapClientOptionsBuilder::createWithDefaults(),
-            SoapOptionsBuilder::createWithDefaults('http://www.webservicex.net/uklocation.asmx?WSDL')
-        );
-        $myRequest = new \stdClass();
-        $myRequest->Town = 'London';
-        $soapResponse = $soapClient->soapCall('GetUKLocationByTown', [$myRequest]);
-
-        //var_dump($soapResponse); // Contains Response, Attachments
-        //var_dump($soapResponse->getContentDocument());
-
-        $document = $soapResponse->getContentDocument();
-
-        $crawler = new Crawler($document->textContent);
-
-        $crawler = $crawler->filterXPath('//table');
-
-        $data = $this->parseDom($crawler);
-
-        
 
 
 
     }
 
-    private function parseDom(Crawler $dom) {
+    private function echoPostCodes($data) {
+        foreach ($data as $item) {
+            echo $item['postcode']. ' - ' . $item['town'] . ' - ' . $item['county']. "\n";
+        }
+    }
+
+    private function requestPostCodeByTown($town, SoapClient $soapClient) {
+            $myRequest = new \stdClass();
+            $myRequest->Town = $town;
+            $soapResponse = $soapClient->soapCall(self::METHOD_NAME, [$myRequest]);
+
+            $document = $soapResponse->getContentDocument();
+
+            $crawler = new Crawler($document->textContent);
+
+            $crawler = $crawler->filterXPath(self::RESPONSE_ITEM_XPATH);
+
+            $data = $this->parseDom($crawler);
+
+            return $data;
+    }
+
+    private static function parseDom(Crawler $dom) {
         // adjusted from https://stackoverflow.com/questions/38065659/how-to-parse-html-table-to-array-with-symfony-dom-crawler
         $ret = [];
         foreach ($dom as $content) {
             $item = array();
             $crawler = new Crawler($content);
-            foreach ($crawler->filterXPath('//table/*') as $node) {
+            foreach ($crawler->filterXPath(self::RESPONSE_DATA_XPATH) as $node) {
                 $item[$node->nodeName] = $node->nodeValue;
             }
             $ret[] = $item;
